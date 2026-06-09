4058e431455ac63e5d4226358ff0193081ff3ed9
Mehul <mehul.lemolite@gmail.com>
2026-06-09 11:02:09 +0530

    Fulfillment page
    Fulfillment page code removed for temporary basis

--- src/components/ReusableComponent/Header/Header.jsx
+++ src/components/ReusableComponent/Header/Header.jsx
@@ -191,7 +191,7 @@ export default function Header() {
             {
               name: "Fulfillment Center",
               icon: <FulfillmentIcon />,
-              href: "/Infra/FulfillmentCenter",
+              href: "/infra/fulfillmentCenter",
             },
             {
               name: "Deliveries",
